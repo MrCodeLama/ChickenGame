2302f6193127853f33cf4fb9afb595c57561ba68
Volodymyr Demianchuk <dem.volodymyrrr@gmail.com>
2023-10-02 20:35:47 +0300

    added jumping of character

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public float Speed = 10f;
+   public Rigidbody rb;
+   public bool _isOnGround = true;
+
+   private void Start()
+   {
+      rb = GetComponent<Rigidbody>();
+   }
+
+   private void Update()
+   {
+      if (Input.GetMouseButtonDown(0) && _isOnGround)
+      {
+         rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+         _isOnGround = false;
+      }
+   }
+
+   private void OnCollisionEnter(Collision col)
+   {
+      if (col.gameObject.name == "Ground")
+      {
+         _isOnGround = true;
+      }
+   }
+}
